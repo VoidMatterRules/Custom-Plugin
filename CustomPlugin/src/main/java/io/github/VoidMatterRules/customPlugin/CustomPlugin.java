a04b22c6ee403bd419f488e1c9f0407de07a69b4
VoidMatterRules <119881062+VoidMatterRules@users.noreply.github.com>
2024-09-11 14:26:04 +0200

    Project Overwrite

---
+++ CustomPlugin/src/main/java/io/github/VoidMatterRules/customPlugin/CustomPlugin.java
@@ -0,0 +1,18 @@
+package io.github.VoidMatterRules.customPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
+
